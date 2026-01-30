2a602b02d318b211034d519824c71dc9ca3cbd68
gaboliveiradev <g38789138@gmail.com>
2026-01-29 23:16:46 -0300

    feat: add dynamic export to budgets and clients page components

--- app/app/budgets/page.tsx
+++ app/app/budgets/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Header from "@/components/common/Header";
 import BudgetsFormModal from "./budgets-form-modal";
 import { getBudgets } from "@/actions/budgets/get-budgets.action";
